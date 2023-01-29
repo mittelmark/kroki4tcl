0783b1e3530b74be4303aa6778ea5ecd1e5fb799
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-01-29 18:14:54 +0100

    setup added

---
+++ setup.tcl
@@ -0,0 +1,11 @@
+# setup file for tclinstaller.tcl
+
+array set setup {
+  name kroki4tcl
+  version 0.4.0
+  url https://github.com/mittelmark/kroki4tcl
+  author {Detlef Groth}
+  license {BSD 3.0}
+  include {kroki4tcl/*.tcl kroki4tcl/kroki4tcl.md}
+}
+
