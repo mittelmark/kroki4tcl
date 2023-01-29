f901bc37afe005cd421be23584f924ca33b144a4
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-01-29 09:49:42 +0100

    adding more spacing in api markdown output

--- kroki4tcl/kroki4tcl_api.tcl
+++ kroki4tcl/kroki4tcl_api.tcl
@@ -17,15 +17,18 @@ if [catch {open $filename r} infh] {
         if {[regexp {^\s*#'} $line]} {
             set nline [regsub {^\s*#' ?} $line ""]
             if {$color} {
+                if {![regexp {^#} $nline]} {
+                    set nline "  $nline"
+                }
                 set nline [regsub {^(#.+)} $nline "\033\[01;36m\\1\033\[0m"]
                 set nline [regsub {(\*\*.+?\*\*)} $nline "\033\[01;33m\\1\033\[0m"]
                 set nline [regsub {(__.+?__)} $nline "\033\[01;33m\\1\033\[0m"]
                 set nline [regsub {( [\*_][^\s*_].+?[\*_])} $nline "\033\[01;34m\\1\033\[0m"]
                 set nline [regsub {^> } $nline "  "]
-                if {$example && [regexp {^ {0,2}```} $nline]} {
+                if {$example && [regexp {^ {0,4}```} $nline]} {
                     set nline "$nline\033\[0m"
                     set example false
-                } elseif {[regexp {^ {0,2}```} $nline]} {
+                } elseif {[regexp {^ {0,4}```} $nline]} {
                     set nline "\033\[01;31m$nline"
                     set example true
                 }
